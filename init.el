4cd73bbd06dad41e26530952396635d0690d189b
Evan Lawrence-Hurt <evan.lawrence-hurt@itg.com>
2014-06-12 16:35:45 -0400

    add zshrc and windows init.el, modify dotemacs.el to work with unix emacs

--- init.el
+++ init.el
@@ -14,28 +14,6 @@
                     (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path "~/.emacs.d/")
 
-;; Make sure the path is set up for programs launched
-;; via Spotlight, the Dock, Finder, &c, by running:
-;; $ defaults write $HOME/.MacOSX/environment PATH "$PATH"
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; OS INTEGRATION
-
-;; use OS X's Spotlight for M-x locate
-(setq locate-make-command-line (lambda (s) `("mdfind" "-name" ,s)))
-
-;; I'll be sending files from the command line
-(server-start)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ENCODING
-
-;; always utf-8, all the time
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq slime-net-coding-system 'utf-8-unix)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PACKAGES
 
 ;; more (and more up-to-date) packages than plain ELPA
@@ -46,7 +24,7 @@
 (package-initialize)
 
 ;; this approached is taken from Prelude
-(defvar evanlh-packages '(ac-nrepl projectile dired+ helm-projectile icicles helm ack-and-a-half ac-slime auto-complete clojure-mode coffee-mode color-theme-sanityinc-tomorrow css-mode elisp-slime-nav expand-region find-file-in-project go-mode haml-mode haskell-mode idle-highlight-mode ido-ubiquitous inf-ruby js2-mode js2-refactor magit markdown-mode molokai-theme paredit popup powerline ruby-block ruby-end ruby-mode skewer-mode slime slime-ritz smex starter-kit starter-kit-eshell starter-kit-js starter-kit-lisp starter-kit-ruby twilight-theme undo-tree yaml-mode ein cider))
+(defvar evanlh-packages '(ac-nrepl projectile helm-projectile icicles helm ack-and-a-half ac-slime auto-complete clojure-mode coffee-mode color-theme-sanityinc-tomorrow css-mode elisp-slime-nav expand-region find-file-in-project haml-mode haskell-mode idle-highlight-mode ido-ubiquitous inf-ruby js-comint yasnippet js2-mode js2-refactor magit markdown-mode molokai-theme paredit popup powerline ruby-block ruby-end ruby-mode skewer-mode slime slime-ritz smex starter-kit starter-kit-eshell starter-kit-js starter-kit-lisp starter-kit-ruby twilight-theme undo-tree yaml-mode ein))
 
 (defun evanlh-packages-installed-p ()
   (loop for p in evanlh-packages
@@ -62,14 +40,207 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
+;; Make sure the path is set up for programs launched
+;; via Spotlight, the Dock, Finder, &c, by running:
+;; $ defaults write $HOME/.MacOSX/environment PATH "$PATH"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; OS INTEGRATION
+
+;; use OS X's Spotlight for M-x locate
+;;(setq locate-make-command-line (lambda (s) `("mdfind" "-name" ,s)))
+
+(if (equal system-type 'windows-nt)
+    (progn
+      ;; (setq explicit-shell-file-name
+      ;;       "C:/Program Files (x86)/Git/bin/sh.exe")
+      ;; (setq shell-file-name "bash")
+      ;; (setq explicit-sh.exe-args '("--login" "-i"))
+      ;; (setenv "SHELL" shell-file-name)
+      ;; (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
+      (setq url-proxy-services
+            '(("http"     . "adm-wp.hq.itginc.com:8080")
+              ("ftp"      . "adm-wp.hq.itginc.com:8080")
+              ("https"    . "adm-wp.hq.itginc.com:8080")
+              ("no_proxy" . "^.*example.com")))
+      (setenv "PATH"
+              (concat
+               "C:/cygwin64/usr/local/bin" ";"
+               "C:/cygwin64/usr/bin" ";"
+               "C:/cygwin64/bin" ";"
+               (getenv "PATH")
+               ))
+      ;; keys for windows
+      (setq w32-pass-lwindow-to-system nil
+            w32-pass-rwindow-to-system nil
+            w32-pass-apps-to-system nil
+            w32-lwindow-modifier 'super ; Left Windows key
+            w32-rwindow-modifier 'super ; Right Windows key
+            w32-apps-modifier 'hyper)
+      (add-to-list 'exec-path "C:/cygwin64/bin")
+      (add-to-list 'exec-path "C:/cygwin64/usr/bin")
+      (add-to-list 'exec-path "C:/cygwin64/usr/local/bin")
+      (setq shell-file-name "bash")
+      (setq explicit-sh.exe-args '("--login" "-i"))
+      (setq explicit-shell-file-name shell-file-name)
+
+      ;; Mouse wheel behavior
+      (global-set-key [C-wheel-up] 'text-scale-increase)
+      (global-set-key [C-wheel-down] 'text-scale-decrease)
+      (global-set-key [C-down-mouse-2] 'text-scale-normal-size)
+
+      ;; automatically copy text when mouse drag. Similar to Linux X11 behavior
+      (setq mouse-drag-copy-region t)
+      ;; when pasting with linux x11 middle click, set to paste at cursor position, not at click position
+      (setq mouse-yank-at-point t)
+      (cua-mode t)
+      (global-set-key (kbd "<M-up>") 'beginning-of-buffer)
+      (global-set-key (kbd "<M-down>") 'end-of-buffer)
+
+      (add-to-list 'load-path "~/lintnode")
+      (require 'flymake-jslint)
+      ;; Make sure we can find the lintnode executable
+      (setq lintnode-location "~/lintnode")
+      ;; JSLint can be... opinionated
+      (setq lintnode-jslint-excludes (list 'nomen 'undef 'plusplus 'onevar 'white))
+
+      (setq inferior-js-program-command "node")
+      (setq inferior-js-mode-hook
+      (lambda ()
+        ;; We like nice colors
+        (ansi-color-for-comint-mode-on)
+        ;; Deal with some prompt nonsense
+        (add-to-list 'comint-preoutput-filter-functions
+                     (lambda (output)
+                       (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
+                     (replace-regexp-in-string ".*1G.*3G" "&gt;" output))))
+        ))
+      (require 'windata)
+      (require 'dirtree)
+      ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; INPUT MAPPING
+
+;;;; Normalize with Mac OS X
+;; command + up/down/left/right = file start/end, line start/end
+
+;; commenting command-up because I often use it
+(global-set-key (kbd "<s-left>") 'move-beginning-of-line)
+(global-set-key (kbd "<s-right>") 'move-end-of-line)
+
+(global-set-key (kbd "<s-mouse-1>") 'mouse-major-mode-menu)
+
+;; I prefer C-s-up and C-s-down so I don't do it accidentally
+(global-set-key (kbd "<C-s-up>") 'beginning-of-buffer)
+(global-set-key (kbd "<C-s-down>") 'end-of-buffer)
+
+(setq shift-select-mode t) ; shift-select mode
+(setq delete-selection-mode 1)  ; typing after selection kills the region
+
+ ;; Mac OS X-style font-size control
+(define-key global-map (kbd "s-+") 'text-scale-increase)
+(define-key global-map (kbd "s--") 'text-scale-decrease)
+
+;; alt-click for mouse-2, command-click for mouse-3
+;; broken?
+(setq mac-emulate-three-button-mouse t)
+
+;; Smoother scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed 1) ;; accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
+;; fn+option+delete = kill word to the right in OS X inputs
+;; (iterm2 ignores the option modifier)
+(define-key global-map (kbd "<M-kp-delete>") 'paredit-forward-kill-word)
+
+;; undo-tree-mode with aliases that match OS X undo/redo
+(require 'undo-tree)
+(global-undo-tree-mode 1)
+(defalias 'redo 'undo-tree-redo)
+
+(global-set-key (kbd "s-z") 'undo) ; command+z
+(global-set-key (kbd "s-Z") 'redo) ; shift+command+z
+
+;; command-f, the default OSX search keybinding =>  forward search
+(global-set-key (kbd "s-f") 'isearch-forward)
+;; command-r, forward-replace
+(global-set-key (kbd "s-r") 'query-replace-regexp)
+
+;; OS X Lion fullscreen mode command-return
+(global-set-key (kbd "<s-return>") 'ns-toggle-fullscreen)
+
+;;;; Normalize with the shell
+;; make M-up and M-down the same as C-up and C-down
+(global-set-key (kbd "<M-up>") 'backward-paragraph)
+(global-set-key (kbd "<M-down>") 'forward-paragraph)
+(global-set-key (kbd "<M-right>") 'end-of-line)
+(global-set-key (kbd "<M-left>") 'beginning-of-line)
+
+
+;; like in the shell
+(global-set-key (kbd "C-d") 'delete-forward-char)
+
+;; normalize with inputrc's C-w, use Mac-style command-X for cut region
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+;; add readline's backward-kill-line
+(defun backward-kill-line ()
+  "kill from point to the start of line"
+  (interactive)
+  (kill-line 0))
+(global-set-key (kbd "C-x <C-backspace>") 'backward-kill-line)
+
+;; prefer regexp in my backward search, inputrc-compatible binding
+(global-set-key (kbd "^R") 'isearch-backward-regexp)
+
+;; moving between windows, normalize with iTerm2 and (mod'd) tmux
+(global-set-key [M-s-left] 'windmove-left)
+(global-set-key [M-s-right] 'windmove-right)
+(global-set-key [M-s-up] 'windmove-up)
+(global-set-key [M-s-down] 'windmove-down)
+
+;; enhanced completion library, same as inputrc binding
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; expand-region is super handy while editing code
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; turn off safety mode
+(put 'downcase-region 'disabled nil)
+
+;; delete selected text if you hit backspace or del
+(delete-selection-mode 1)
+
+
+;; I'll be sending files from the command line
+(server-start)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ENCODING
+
+;; always utf-8, all the time
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq slime-net-coding-system 'utf-8-unix)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; APPEARANCE
 
 ;; split new windows horizontal
 ;; (http://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal)
-(setq split-height-threshold 0)
+(setq split-height-threshold nil)
 (setq split-width-threshold nil)
 
-;; show menu
+;; disable autofill mode
+(auto-fill-mode -1)
+(remove-hook 'text-mode-hook #'turn-on-auto-fill)
+(remove-hook 'js2-mode-hook #'turn-on-auto-fill)
+
+; show menu
 (menu-bar-mode 1)
 ;; turn off splash screen messages
 (setq inhibit-startup-echo-area-message t
@@ -118,11 +289,11 @@
 (global-hl-line-mode -1)
 
 ;; bring on the color theme
-(color-theme-sanityinc-tomorrow-bright)
+(color-theme-sanityinc-tomorrow-night)
 
 ;; powerline gives a much aesthetically improved mode line, the look
 ;; of which is stolen from vi.
-;;(require 'powerline)
+(require 'powerline)
 ;;(powerline-default)
 
 ;; I hate the box on the mode-line
@@ -133,95 +304,9 @@
                     :box nil
                     :background "#000040")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; INPUT MAPPING
-
-;;;; Normalize with Mac OS X
-;; command + up/down/left/right = file start/end, line start/end
-
-;; commenting command-up because I often use it
-(global-set-key (kbd "<s-left>") 'move-beginning-of-line)
-(global-set-key (kbd "<s-right>") 'move-end-of-line)
-(global-set-key (kbd "<s-mouse-1>") 'mouse-major-mode-menu)
-
-;; I prefer C-s-up and C-s-down so I don't do it accidentally
-(global-set-key (kbd "<C-s-up>") 'beginning-of-buffer)
-(global-set-key (kbd "<C-s-down>") 'end-of-buffer)
-
-(setq shift-select-mode t) ; shift-select mode
-(delete-selection-mode 1)  ; typing after selection kills the region
-
-;; Mac OS X-style font-size control
-(define-key global-map (kbd "s-+") 'text-scale-increase)
-(define-key global-map (kbd "s--") 'text-scale-decrease)
-
-;; alt-click for mouse-2, command-click for mouse-3
-;; broken?
-(setq mac-emulate-three-button-mouse t)
-
-;; Smoother scrolling
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-(setq mouse-wheel-progressive-speed 1) ;; accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll one line at a time
-
-;; fn+option+delete = kill word to the right in OS X inputs
-;; (iterm2 ignores the option modifier)
-(define-key global-map (kbd "<M-kp-delete>") 'paredit-forward-kill-word)
-
-;; undo-tree-mode with aliases that match OS X undo/redo
-(require 'undo-tree)
-(global-undo-tree-mode 1)
-(defalias 'redo 'undo-tree-redo)
-(global-set-key (kbd "s-z") 'undo) ; command+z
-(global-set-key (kbd "s-Z") 'redo) ; shift+command+z
-
-;; command-f, the default OSX search keybinding =>  forward search
-(global-set-key (kbd "s-f") 'isearch-forward)
-;; command-r, forward-replace
-(global-set-key (kbd "s-r") 'query-replace-regexp)
-
-;; OS X Lion fullscreen mode command-return
-(global-set-key (kbd "<s-return>") 'ns-toggle-fullscreen)
-
-;;;; Normalize with the shell
-;; make M-up and M-down the same as C-up and C-down
-(global-set-key (kbd "<M-up>") 'backward-paragraph)
-(global-set-key (kbd "<M-down>") 'forward-paragraph)
-
-;; like in the shell
-(global-set-key (kbd "C-d") 'delete-forward-char)
-
-;; normalize with inputrc's C-w, use Mac-style command-X for cut region
-(global-set-key (kbd "C-w") 'backward-kill-word)
-
-;; add readline's backward-kill-line
-(defun backward-kill-line ()
-  "kill from point to the start of line"
-  (interactive)
-  (kill-line 0))
-(global-set-key (kbd "C-x <C-backspace>") 'backward-kill-line)
-
-;; prefer regexp in my backward search, inputrc-compatible binding
-(global-set-key (kbd "^R") 'isearch-backward-regexp)
-
-;; moving between windows, normalize with iTerm2 and (mod'd) tmux
-(global-set-key [M-s-left] 'windmove-left)
-(global-set-key [M-s-right] 'windmove-right)
-(global-set-key [M-s-up] 'windmove-up)
-(global-set-key [M-s-down] 'windmove-down)
-
-;; enhanced completion library, same as inputrc binding
-(global-set-key (kbd "M-/") 'hippie-expand)
-
-;; expand-region is super handy while editing code
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-;; turn off safety mode
-(put 'downcase-region 'disabled nil)
-
-;; delete selected text if you hit backspace or del
-(delete-selection-mode t)
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PROGRAMMING/LANGUAGES
 
@@ -246,17 +331,15 @@
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; ac-mode for nrepl for clojure
-;;(require 'ac-nrepl)
-;;(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-;;(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-
-
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
 
-;;(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;;(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;;(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode) 
 ;;(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 ;; command-k compile shortcut
@@ -321,7 +404,7 @@
 ;; this is needed to prevent ac-nrepl from breaking
 ;; clojure-mode's starting of nrepl-interaction mode
 ;; on nrepl-jack-in
-;; (setq nrepl-connected-hook (reverse nrepl-connected-hook))
+(setq nrepl-connected-hook (reverse nrepl-connected-hook))
 
 ;; disable stack traces outside of repl
 ;;(setq nrepl-popup-stacktraces nil)
@@ -343,7 +426,19 @@
 (require 'js2-mode)
 (setq-default js2-auto-indent-p t)
 (setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON"))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
+
+(add-hook 'javascript-mode-hook
+          (lambda () (flymake-mode t)))
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (setq js2-basic-offset 2)
+            (setq js2-bounce-indent-p t)))
+
+(setq js2-basic-offset 2)
+(setq js-indent-level 2)
+(setq c-basic-offset 2)
+(setq indent-tabs-mode nil)
 
 ;; skewer mode for browser mind control
 (require 'skewer-mode)
@@ -385,7 +480,7 @@
 ;; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 
-;; (add-to-list 'auto-mode-alist '("\\.php" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.php" . php-mode))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Projectile
@@ -396,7 +491,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; EIN
 (setq ein:use-auto-complete t)
-
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; php-mode
@@ -471,7 +565,7 @@
 
 (setq ffip-patterns '("*.html" "*.org" "*.md" "*.el" "*.clj" "*.py" "*.rb" "*.js" "*.pl" "*.sh" "*.erl" "*.hs" "*.ml" "*.php" "*.html" "*.phtml"))
 (setq ffip-limit 4096)
-(toggle-diredp-find-file-reuse-dir 1)
+;;(toggle-diredp-find-file-reuse-dir 1)
 (global-auto-revert-mode)
 (global-set-key (kbd "C-x f") 'find-file-in-project)
 
@@ -493,15 +587,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21"))
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
- '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
- '(exec-path (quote ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin"))))
+ '(custom-safe-themes (quote ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fci-rule-color "#282a2e")
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#cc6666") (40 . "#de935f") (60 . "#f0c674") (80 . "#b5bd68") (100 . "#8abeb7") (120 . "#81a2be") (140 . "#b294bb") (160 . "#cc6666") (180 . "#de935f") (200 . "#f0c674") (220 . "#b5bd68") (240 . "#8abeb7") (260 . "#81a2be") (280 . "#b294bb") (300 . "#cc6666") (320 . "#de935f") (340 . "#f0c674") (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
 
 
 
