2c24b04ae268a431ab4cc848bc5acc7da07ec6a9
Evan Lawrence-Hurt <evanlh@gmail.com>
2016-05-02 23:29:02 -0400

    exec path for os x

--- my-emacs/my-darwin.el
+++ my-emacs/my-darwin.el
@@ -21,6 +21,13 @@
       ;; broken?
       (setq mac-emulate-three-button-mouse t)
 
+      ;; fix the PATH variable
+      (defun set-exec-path-from-shell-PATH ()
+        (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+          (setenv "PATH" path-from-shell)
+          (setq exec-path (split-string path-from-shell path-separator))))
+
+      (when window-system (set-exec-path-from-shell-PATH))
       ;; use OS X's Spotlight for M-x locate
 
       (setq locate-make-command-line (lambda (s) `("mdfind" "-name" ,s))))
