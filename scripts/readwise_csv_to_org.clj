1c00a89eb02a9a5d02a141ab53aea36a6c1abcaf
Evan Lawrence-Hurt <evanlh@gmail.com>
2022-03-13 20:15:44 -0400

    first version of script for exporting readwise csv export to org-mode for easier browsing

---
+++ scripts/readwise_csv_to_org.clj
@@ -0,0 +1,47 @@
+#!/usr/bin/env bb
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
+(def readwise-seq (with-open [reader (io/reader "/Users/elh/Downloads/readwise-data.csv")]
+                    (doall
+                     (csv/read-csv reader))))
+
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+	  (rest csv-data)))
+
+(def readwisemap (csv-data->maps readwise-seq))
+
+(def book-id (keyword "Amazon Book ID"))
+(def book-title (keyword "Book Title"))
+(def book-author (keyword "Book Author"))
+(def highlighted-at (keyword "Highlighted at"))
+(def highlight (keyword "Highlight"))
+(def highlight-location (keyword "Location"))
+(def highlight-color (keyword "Color"))
+
+(def book-key [book-id book-title book-author])
+
+(def book-to-highlights-map (group-by #(select-keys % book-key) readwisemap))
+
+(defn filename-str [k]
+  (format  "%s - %s - %s.org" (book-title k) (book-author k) (book-id k)))
+
+(defn title-str [k]
+  (format "* %s - %s\n" (book-title k) (book-author k)))
+
+(defn highlight-str [l]
+  (format "** Location %s - %s\n%s\n" (highlight-location l) (highlighted-at l) (highlight l)))
+
+(defn highlight-loc-num [r] (bigdec (highlight-location r)))
+
+(defn highlights-str [k] (reduce str (map highlight-str (sort #(compare (highlight-loc-num %1) (highlight-loc-num %2)) (book-to-highlights-map k)))))
+
+(def book-title-keys (sort #(compare (title-str %1) (title-str %2)) (keys book-to-highlights-map)))
+(def output (reduce str (map #(str (title-str %) (highlights-str %)) book-title-keys)))
+
+(spit "output.org" output)
+
