9eb6ba57e5537b92f21523bb2cd8c68509727645
Evan Lawrence-Hurt <evanlh@gmail.com>
2023-06-16 19:54:39 +0200

    readwise to org-mode script updates: generate ID from location, title includes highlight text

--- scripts/readwise_csv_to_org.clj
+++ scripts/readwise_csv_to_org.clj
@@ -16,6 +16,8 @@
 
 (def readwisemap (csv-data->maps readwise-seq))
 
+
+
 (def book-id (keyword "Amazon Book ID"))
 (def book-title (keyword "Book Title"))
 (def book-author (keyword "Book Author"))
@@ -23,6 +25,9 @@
 (def highlight (keyword "Highlight"))
 (def highlight-location (keyword "Location"))
 (def highlight-color (keyword "Color"))
+(def tags (keyword "Tags"))
+
+;; (def rwt (filter #(> (count ((keyword "Tags") %)) 0) readwisemap))
 
 (def book-key [book-id book-title book-author])
 
@@ -31,12 +36,30 @@
 (defn filename-str [k]
   (format  "%s - %s - %s.org" (book-title k) (book-author k) (book-id k)))
 
+(defn org-id-from-record [k] (str "RW-" (book-id k) "-L" (highlight-location k)))
+
+(defn org-prop-tag [k]
+  (let [tagstr (tags k)]
+    (if (> (count tagstr) 0)
+      (format "TAGS:       %s\n" tagstr)
+      "")))
+
+(defn org-props-str [k] (format ":PROPERTIES:\n:ID:       %s\n:TIMESTAMP:       %s\n:%s\n:END:"
+                                (org-id-from-record k) (subs (highlighted-at k) 0 10) (org-prop-tag k)))
+
+;; (org-props-str (first rwt))
+;; (org-props-str (first readwisemap))
+
 (defn title-str [k]
   (format "* %s - %s\n" (book-title k) (book-author k)))
 
+(defn highlight-title-str [l]
+  (let [c (count (highlight l))]
+    (str (subs (highlight l) 0 (min 50 c)) (if (> c 50) "..."))))
+
 (defn highlight-str [l]
-  (format "** [[shell:open 'kindle://book?action=open&asin=%s&location=%s'][Location %s]] - %s\n%s\n"
-          (book-id l) (highlight-location l) (highlight-location l) (subs (highlighted-at l) 0 10) (highlight l)))
+  (format "** [[shell:open 'kindle://book?action=open&asin=%s&location=%s'][%s]]\n%s\n%s\n"
+          (book-id l) (highlight-location l) (highlight-title-str l) (org-props-str l) (highlight l)))
 
 (defn highlight-loc-num [r] (bigdec (highlight-location r)))
 
@@ -45,4 +68,4 @@
 (def book-title-keys (sort #(compare (title-str %1) (title-str %2)) (keys book-to-highlights-map)))
 (def output (reduce str (map #(str (title-str %) (highlights-str %)) book-title-keys)))
 
-(spit *out*  output)
+(spit *out* output)
