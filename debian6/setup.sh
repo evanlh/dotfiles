d6af3d4c563d3b0d6a95adb3af291b1ab03048c5
Evan Lawrence-Hurt <evanlh@gmail.com>
2024-11-30 07:49:37 -0800

    match gnome wm keybindings to dotslate keybindings

--- debian6/setup.sh
+++ debian6/setup.sh
@@ -33,14 +33,6 @@ rm ~/.gitconfig; ln -s ./dotfiles/dotgitconfig ~/.gitconfig
 rm ~/.gitignore; ln -s ./dotfiles/dotgitignore_global ~/.gitignore
 git config --global core.excludesFile '~/.gitignore'
 
-# gnome config
-gsettings set org.gnome.desktop.interface enable-animations false
-
-gsettings set org.gnome.shell.keybindings toggle-overview '[]'
-gsettings set org.gnome.shell.keybindings toggle-message-tray '[]'
-gsettings set org.gnome.shell.keybindings shift-overview-down '[]'
-gsettings set org.gnome.shell.keybindings shift-overview-up '[]'
-
 # foot terminal config
 mkdir -p ~/.config/foot
 ln -s ~/dotfiles/debian6/foot.ini ~/.config/foot
@@ -49,4 +41,54 @@ ln -s ~/dotfiles/debian6/foot.ini ~/.config/foot
 mkdir -p ~/.emacs.d/
 ln -s ~/dotfiles/dotemacs.el ~/.emacs.el
 ln -s ~/dotfiles/my-emacs/ ~/.emacs.d/
+
+# gnome config
+gsettings set org.gnome.desktop.interface enable-animations false
+
+# gnome shell keybindings
+gsettings set org.gnome.shell.keybindings toggle-overview '[]'
+gsettings set org.gnome.shell.keybindings toggle-message-tray '[]'
+gsettings set org.gnome.shell.keybindings shift-overview-down '[]'
+gsettings set org.gnome.shell.keybindings shift-overview-up '[]'
+
+# gnome wm keybindings
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down []
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up []
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-last []
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control>Right']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "[]"
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Control><Alt>Space']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "[]"
+gsettings set org.gnome.desktop.wm.keybindings maximize-horizontally []
+gsettings set org.gnome.desktop.wm.keybindings maximize-vertically []
+gsettings set org.gnome.desktop.wm.keybindings minimize []
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-ne "['<Control><Alt>O']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-nw "['<Control><Alt>U']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-se "['<Control><Alt>period']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-sw "['<Control><Alt>M']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Control><Alt>Left', '<Control><Alt>J']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Control><Alt>Right', '<Control><Alt>L']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-n "['<Control><Alt>i']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-s "['<Control><Alt>comma']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "[]"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "[]"
+
+# gnome keybindings rebind super-# to switch workspaces
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
 
