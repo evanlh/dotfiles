5fc0cde11644b991aed6f3a3302bee2f9bcc234e
Evan Lawrence-Hurt <elawrencehur@bloomberg.net>
2025-01-07 12:19:30 +0000

    disable default os x browser post v29 upgrade

--- my-emacs/my-darwin.el
+++ my-emacs/my-darwin.el
@@ -23,9 +23,6 @@
       ;; broken?
       (setq mac-emulate-three-button-mouse t)
 
-	  ;; use mac browser
-	  (setq browse-url-browser-function 'browse-default-macosx-browser)
-
       ;; fix the PATH variable
       (defun set-exec-path-from-shell-PATH ()
         (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
